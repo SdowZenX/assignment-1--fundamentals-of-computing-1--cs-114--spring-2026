d347467b7209e00c789bd84f5e785cb7139bd894
SdowZenX <zanes11zs@gmail.com>
2026-02-19 16:59:14 -0500

    Assignment 1

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+import java.util.Random;
 public class Assignment1Program1 {
   public static void main(String[] args) {
+System.out.println("ZZZZZZZ   SSSSS");
+System.out.println("     ZZ  SS    ");
+System.out.println("    ZZ    SSS  ");
+System.out.println("   ZZ        SS");
+System.out.println("  ZZ         SS");
+System.out.println(" ZZ      SS   S");
+System.out.println("ZZZZZZZ   SSSSS");
+System.out.println();
 
+Scanner input = new Scanner(System.in);
+System.out.print("Please enter a 5-character string:");
+String str = input.nextLine();
+String trimmed = str.substring(1,4);
+String reversed = new StringBuilder(trimmed).reverse().toString();
+System.out.println();
+
+System.out.print("Please enter a number in Fahrenheit:");
+double fahrenheit = input.nextDouble();
+double celsius = (fahrenheit - 32) * 5 / 9;
+System.out.println();
+
+Random rand = new Random();
+System.out.println("Random number generated. Continuing   ");
+int randomNum = rand.nextInt(16353) + 32;
+System.out.println();
+
+String result = String.format("%.1f", celsius) + "." + reversed + randomNum;
+System.out.println("Your new string is " + result);
+input.close();
   }
 }
--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class Assignment1Program2 {
   public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+System.out.println("Base Conversion Program");
+System.out.println();
 
+System.out.print("Please enter a base (2-9):");
+int base = input.nextInt();
+int maxNumber = (base - 1) * base * base * base + (base - 1) * base * base + (base - 1) * base +(base - 1);
+
+System.out.println("The maximum, 4 digit, base 10 number in base" + base + " is "+ maxNumber + ".");
+System.out.print("Now, enter a base 10 number in the range 0 to " + maxNumber + " to convert: ");
+int num = input.nextInt();
+System.out.println();
+
+int digit1 = num / (base * base * base);
+int remainder1 = num % (base * base * base);
+int digit2 = remainder1 / (base * base);
+int remainder2 = remainder1 % (base * base);
+int digit3 = remainder2 / base;
+int digit4 = remainder2 % base;
+System.out.println(num + " (base 10) = " + digit1 + digit2 + digit3 + digit4 + " (base " + base + ")");
+input.close();
   }
 }
